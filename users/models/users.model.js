554ed324c7d63ee9aea7fe910d53cfbe0290958a
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2021-04-22 11:24:46 +0200

    set email to be lowercase

--- users/models/users.model.js
+++ users/models/users.model.js
@@ -14,7 +14,8 @@ const userSchema = new Schema({
         type: String,
         required: true,
         trim: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password: {
         type: String,
