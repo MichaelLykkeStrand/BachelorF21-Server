5b7648a7c4904a7fa45b1da46e12f1a646811b9e
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2021-05-17 13:26:21 +0200

    Update progress.controller.js

--- progress/controller/progress.controller.js
+++ progress/controller/progress.controller.js
@@ -2,7 +2,7 @@ const ProgressModel = require('../models/progress.model');
 
 
 exports.getProgressByTaskIDAndUserID = (req, res) => {
-    ProgressModel.findByTaskIdAndUserID(req.params.taskID, req.params.userId)
+    ProgressModel.findByTaskIdAndUserID(req.params.taskId, req.params.userId)
         .then((result) => {
             res.status(200).send(result);
         });
